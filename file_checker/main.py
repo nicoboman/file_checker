7a53e36b895f78c2560a087944f3437ee809d5ce
nicolas <nicolas@myPc>
2019-03-27 21:49:26 +0100

    implémentation d'un 'menu' simplifié.

--- file_checker/main.py
+++ file_checker/main.py
@@ -11,100 +11,104 @@ from checks.checkID import *
 from checks.checkFdir import *
 from error.datdutErrors import *
 
-print('Name of directory to check:')
-directory = input()
-dir_to_check = C_DAT_DUT_DIR + directory + '\\'
+while True:
+    print('Name of directory to check [q to quit]:')
+    op_choice = input()
+    if op_choice != 'q':
+        directory = op_choice + '\\'
+    else:
+        print('Leaving file checker...')
+        break
+    
+    p_datdut_dir = Path(directory)
 
-p_datdut_dir = Path(C_DAT_DUT_DIR + directory)
-
-# for each dut file in the directory
-for dat_dut_file in p_datdut_dir.glob('*.csv'):
-    displayFileName(dat_dut_file.name)
-    b_id_error_flag = False
-        
-    # opens and checks one file:
-    with open (dir_to_check + dat_dut_file.name,'r',encoding='utf8') as file_handler:
-        # read and check each line of the file
-        for line_number, line in enumerate(file_handler, 1):
-            # end of file
-            if not line:
-                break
-            # do nothing for commentary, blank lines, and first line:
-            elif line.startswith(C_COMMENT) or len(line.strip()) == 0 or line_number == 1: 
-                continue
-            # check the line
-            else:
-                line = line.replace('\n','')
-                liste = line.split(C_SEPARATOR)
-
-                # Checks on the structure of line
-                try:
-                    # Object instanciation
-                    init_checker = CheckInit(liste, line_number, getFUType(dat_dut_file.name))
-                    # First checks
-                    init_checker.checkInit()
-                # main error: unable to determine the FU => move to next file
-                except FilePrefixError:
-                    print('Unable to set the FU type => no additional check for this file\n')
+    # for each dut file in the directory
+    for dat_dut_file in p_datdut_dir.glob('*.csv'):
+        displayFileName(dat_dut_file.name)
+        b_id_error_flag = False
+            
+        # opens and checks one file:
+        with open (directory + dat_dut_file.name,'r',encoding='utf8') as file_handler:
+            # read and check each line of the file
+            for line_number, line in enumerate(file_handler, 1):
+                # end of file
+                if not line:
                     break
-                # important error: line is inconsistent => move to next line
-                except InitialChecksError as e:
-                    print(e.args[0] + 'line ' + str(e.args[1]) + ' => no additional check for this line\n')
-                    b_id_error_flag = True
+                # do nothing for commentary, blank lines, and first line:
+                elif line.startswith(C_COMMENT) or len(line.strip()) == 0 or line_number == 1: 
                     continue
-                # main characteristics are ok => check the line
+                # check the line
                 else:
-                    # Check patterns
-                    if init_checker.getDefinition() == 'PATTERN':
-                        # Check sinus pattern
-                        if init_checker.getType() == 'SINUS':
-                            try:
-                                sinus_pattern_checker = CheckSinusPatterns(init_checker)
-                                sinus_pattern_checker.checkSinusPattern()
-                            except SinusPatternsError as e:
-                                print(e.args[0])
-                        # Check square pattern
-                        elif init_checker.getType() == 'SQUARE':
-                            try:
-                                square_pattern_checker = CheckSquarePatterns(init_checker)
-                                square_pattern_checker.checkSquarePattern()
-                            except SquarePatternsError as e:
-                                print(e.args[0])
-                        # Check bangbang pattern
-                        elif init_checker.getType() == 'BANGBANG':
-                            try:
-                                bangbang_pattern_checker = CheckBangBangPatterns(init_checker)
-                                bangbang_pattern_checker.checkBangBangPattern()
-                            except BangBangPatternsError as e:
-                                print(e.args[0])
-                        # Check trapezoid pattern
-                        elif init_checker.getType() == 'TRAPEZOID':
-                            try:
-                                trapezoid_pattern_checker = CheckTrapezoidPatterns(init_checker)
-                                trapezoid_pattern_checker.checkTrapezoidPattern()
-                            except TrapezoidPatternsError as e:
-                                print(e.args[0])
-                    # Check blocs
-                    elif init_checker.getDefinition() == 'BLOC':
-                            try:
-                                bloc_checker = CheckBlocs(init_checker)
-                                bloc_checker.checkBloc()
-                            except BlocError as e:
-                                print(e.args[0])
-                    # Check fdir
-                    elif init_checker.getDefinition() == 'FDIR':
-                            try:
-                                fdir_checker = CheckFDIR(init_checker)
-                                fdir_checker.checkFdir()
-                            except FdirError as e:
-                                print(e.args[0])
-        # if no error during checks of ids:
-        if not b_id_error_flag:
-            try:
-                id_checker = CheckID(dir_to_check + dat_dut_file.name)
-                id_checker.checkIDs()
-            except IDsError as e:
-                print(e.args[0])
-
-print("\nStrike any key to quit...")
-input()
+                    line = line.replace('\n','')
+                    liste = line.split(C_SEPARATOR)
+    
+                    # Checks on the structure of line
+                    try:
+                        # Object instanciation
+                        init_checker = CheckInit(liste, line_number, getFUType(dat_dut_file.name))
+                        # First checks
+                        init_checker.checkInit()
+                    # main error: unable to determine the FU => move to next file
+                    except FilePrefixError:
+                        print('Unable to set the FU type => no additional check for this file\n')
+                        break
+                    # important error: line is inconsistent => move to next line
+                    except InitialChecksError as e:
+                        print(e.args[0] + 'line ' + str(e.args[1]) + ' => no additional check for this line\n')
+                        b_id_error_flag = True
+                        continue
+                    # main characteristics are ok => check the line
+                    else:
+                        # Check patterns
+                        if init_checker.getDefinition() == 'PATTERN':
+                            # Check sinus pattern
+                            if init_checker.getType() == 'SINUS':
+                                try:
+                                    sinus_pattern_checker = CheckSinusPatterns(init_checker)
+                                    sinus_pattern_checker.checkSinusPattern()
+                                except SinusPatternsError as e:
+                                    print(e.args[0])
+                            # Check square pattern
+                            elif init_checker.getType() == 'SQUARE':
+                                try:
+                                    square_pattern_checker = CheckSquarePatterns(init_checker)
+                                    square_pattern_checker.checkSquarePattern()
+                                except SquarePatternsError as e:
+                                    print(e.args[0])
+                            # Check bangbang pattern
+                            elif init_checker.getType() == 'BANGBANG':
+                                try:
+                                    bangbang_pattern_checker = CheckBangBangPatterns(init_checker)
+                                    bangbang_pattern_checker.checkBangBangPattern()
+                                except BangBangPatternsError as e:
+                                    print(e.args[0])
+                            # Check trapezoid pattern
+                            elif init_checker.getType() == 'TRAPEZOID':
+                                try:
+                                    trapezoid_pattern_checker = CheckTrapezoidPatterns(init_checker)
+                                    trapezoid_pattern_checker.checkTrapezoidPattern()
+                                except TrapezoidPatternsError as e:
+                                    print(e.args[0])
+                        # Check blocs
+                        elif init_checker.getDefinition() == 'BLOC':
+                                try:
+                                    bloc_checker = CheckBlocs(init_checker)
+                                    bloc_checker.checkBloc()
+                                except BlocError as e:
+                                    print(e.args[0])
+                        # Check fdir
+                        elif init_checker.getDefinition() == 'FDIR':
+                                try:
+                                    fdir_checker = CheckFDIR(init_checker)
+                                    fdir_checker.checkFdir()
+                                except FdirError as e:
+                                    print(e.args[0])
+            # if no error during checks of ids:
+            if not b_id_error_flag:
+                try:
+                    id_checker = CheckID(directory + dat_dut_file.name)
+                    id_checker.checkIDs()
+                except IDsError as e:
+                    print(e.args[0])
+                    
+    print('Check is over for ' + op_choice + ' directory.\n')
